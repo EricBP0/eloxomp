54e86d51a9c208e1a96dc8d81d2b3d17be0435b4
ericborges <ericborgesp@gmail.com>
2025-03-16 18:39:27 -0300

    Corrigindo tailwind, adicionando selecionar server e assets, corrigindo precificacao (ainda errada) e ajustes de css para o nabBar e Services.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,20 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import { IoMenu, IoClose } from "react-icons/io5";
-
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <nav className="fixed top-0 left-0 h-full md:w-auto bg-transparent text-white p-4 z-50">
-            <div className="flex justify-end">
-                <button
-                    onClick={() => setMenuOpen(!menuOpen)}
-                    className="text-white text-3xl p-2 md:hidden"
-                >
-                    {menuOpen ? <IoClose /> : <IoMenu />}
-                </button>
-            </div>
+        <nav className="fixed top-0 left-0 w-full bg-gray-800 w-screen text-white p-4 z-50 drop-shadow-lg">
+            <div className="flex justify-start">
+                <img className={"w-20 border-4 border-purple-500"} src="/logo_eloxomp.jpeg" alt="logo"/>
 
-            <ul className={`absolute right-4 top-16 bg-gray-900 p-4 rounded-lg shadow-lg ${menuOpen ? "block" : "hidden"} md:flex md:static md:bg-transparent md:shadow-none md:space-x-6`}>
-                <li>
-                    <Link to="/" className="block py-2 px-4 hover:text-purple-400">Home</Link>
-                </li>
-                <li>
-                    <Link to="/services" className="block py-2 px-4 hover:text-purple-400">Serviços</Link>
-                </li>
-            </ul>
+                <ul className={`absolute right-4 top-16 bg-gray-900 p-4 rounded-lg shadow-lg "block" md:flex md:static md:bg-transparent md:shadow-none md:space-x-6`}>
+                    <li className={"bg-purple-800 rounded-md hover:bg-purple-400"}>
+                        <Link to="/" className="block py-2 px-4 hover:text-purple-800">Home</Link>
+                    </li>
+                    <li className={"bg-purple-800 rounded-md hover:bg-purple-400"}>
+                        <Link to="/services" className="block py-2 px-4 hover:text-purple-800">Serviços</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
